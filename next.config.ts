93a07b20dc467e8924a91f304eb9bdca9c95946b
Sergio Rojas-Aguilar <128770839+Sergrojas29@users.noreply.github.com>
2025-09-24 21:33:20 -0400

    Init commit -added data process to be changed to C++ in near future

--- next.config.ts
+++ next.config.ts
@@ -1,7 +1,8 @@
 import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
-  /* config options here */
+  reactStrictMode: true,  // highlights potential problems
+  // swcMinify: true,        // use SWC compiler for minification (faster builds)
 };
 
 export default nextConfig;
